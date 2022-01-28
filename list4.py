3d614877cf76513938ceaee2bf1fa7a5f14cc32f
Atabey Aydı <64842894+Libldeaera@users.noreply.github.com>
2022-01-28 15:54:26 +0300

    Update list4.py

--- list4.py
+++ list4.py
@@ -4,5 +4,17 @@ return a+b
 #Main Programı
 s=int(input("X Giriniz= "))
 t=int(input("Y Giriniz= "))
-isim=input("Adınızı Giriniz= ")print(isim, topla(t,s), "yaşındadır")
+isim=input("Adınızı Giriniz= ")
+print(isim, topla(t,s), "yaşındadır")
+
+#Recursion
+def yenile(k):
+  if k<=1:
+    return 1
+  else:
+    result = k * yenile(k - 1)
+    return result
+
+print("\n\nRecursion Example Results")
+print(yenile(6))
 
