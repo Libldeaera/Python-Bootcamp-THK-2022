8f2df910db0d5b392b28d6d5aa13d26ba3ee124e
Atabey Aydı <64842894+Libldeaera@users.noreply.github.com>
2022-01-28 15:39:27 +0300

    Create list4.py

---
+++ list4.py
@@ -0,0 +1,8 @@
+#FONKSIYONLARdef topla(a,b):
+return a+b
+
+#Main Programı
+s=int(input("X Giriniz= "))
+t=int(input("Y Giriniz= "))
+isim=input("Adınızı Giriniz= ")print(isim, topla(t,s), "yaşındadır")
+
